7211ce613ab97b929f49b7d61c6a45bc4e811598
vpradhap <pradhapvenkat@gmail.com>
2022-06-03 16:52:18 +0530

    Added : Code to respond happy or sad mood

---
+++ MoodAnalyser.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoodAnalyserProgram
+{
+    public class MoodAnalyser
+    {
+        public string AnalyseMood(string meassage)
+        {
+            if (meassage.ToLower().Contains("sad"))
+            {
+                return "\nSAD MOOD";
+            }
+            else
+            {
+                return "\nHAPPY MOOD";
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,1 +1,7 @@
-﻿Console.WriteLine("\n\t\tMood Analyser program");
+﻿using MoodAnalyserProgram;
+
+Console.WriteLine("\n\t\tMood Analyser program");
+MoodAnalyser moodAnalyser = new MoodAnalyser();
+Console.Write("\nHow are you feeling today : ");
+string message =Console.ReadLine();
+Console.WriteLine(moodAnalyser.AnalyseMood(message));
